da61d3d68b422b0d13676fb397c808ee2280b1d9
heystone999 <52831724+heystone999@users.noreply.github.com>
2024-02-13 18:04:44 -0500

    feat(beckend): user and login

---
+++ memo-backend/app.js
@@ -0,0 +1,22 @@
+const config = require('./utils/config')
+const express = require('express')
+require('express-async-errors')
+const app = express()
+const cors = require('cors')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+const middleware = require('./utils/middleware')
+const mongoose = require('mongoose')
+
+
+mongoose.connect(config.MONGODB_URI)
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
+
+app.use(middleware.errorHandler)
+
+module.exports = app
--- memo-backend/index.js
+++ memo-backend/index.js
@@ -1,8 +1,7 @@
-// index.js
-const app = require('./src/app');  // 引入 Express 应用
+const app = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const port = process.env.PORT || 8080;
-
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
+})
